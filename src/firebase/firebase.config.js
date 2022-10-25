bca3650c8866a613588d1eb1c820857d10e09af4
Sazeduzzaman <sazeduzzaman@yahoo.com>
2022-10-25 14:03:24 +0600

    Finally Site Deployed Successfully

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDJXQFzyRkr52_OynVdhsHcVy3E-3b1Ikg",
+    authDomain: "etutors-7a080.firebaseapp.com",
+    projectId: "etutors-7a080",
+    storageBucket: "etutors-7a080.appspot.com",
+    messagingSenderId: "371261742835",
+    appId: "1:371261742835:web:99a0ef4fe7b2bfe91ca186"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
