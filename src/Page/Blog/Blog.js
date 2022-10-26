1d0b7b552cfc005a5196fb98f84e31bc54edbb7e
Sazeduzzaman <sazeduzzaman@yahoo.com>
2022-10-26 10:40:32 +0600

    Blog And Faq Page Visible

--- src/Page/Blog/Blog.js
+++ src/Page/Blog/Blog.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Accordion from 'react-bootstrap/Accordion';
+import blog from '../../assets/blog.jpg';
+import Iframe from 'react-iframe'
 
 const Blog = () => {
     return (
@@ -8,12 +11,107 @@ const Blog = () => {
                 <div className="container">
                     <div className="pages-banner-content mt-5 mb-5">
                         <h2 className='text-white text-center'>Blog</h2>
+                        <hr />
                         <div className='breadcrumb d-flex justify-content-center'>
                             <span><Link to={'/home'}>Home</Link></span>
                             <span><Link to={'/blog'}>Blog</Link></span>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className="container-fluid mt-5">
+                <div className="container ">
+                    <div className='d-flex justify-content-center align-items-center'>
+                        <img className='w-50' src={blog} alt="" />
+                    </div>
+                    <div className='row mb-5 mt-3'>
+                        <div className='col d-flex justify-content-center align-items-center'>
+                            <Accordion defaultActiveKey={['0']} alwaysOpen className="w-50">
+                                <Accordion.Item eventKey="0">
+                                    <Accordion.Header> What is cors?</Accordion.Header>
+                                    <Accordion.Body>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                        culpa qui officia deserunt mollit anim id est laborum.
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                                <Accordion.Item eventKey="1">
+                                    <Accordion.Header>Why are you using firebase? </Accordion.Header>
+                                    <Accordion.Body>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                        culpa qui officia deserunt mollit anim id est laborum.
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                                <Accordion.Item eventKey="1">
+                                    <Accordion.Header>What other options do you have to implement authentication? </Accordion.Header>
+                                    <Accordion.Body>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                        culpa qui officia deserunt mollit anim id est laborum.
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                                <Accordion.Item eventKey="2">
+                                    <Accordion.Header> How does the private route work?</Accordion.Header>
+                                    <Accordion.Body>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                        culpa qui officia deserunt mollit anim id est laborum.
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                                <Accordion.Item eventKey="3">
+                                    <Accordion.Header>What is Node? How does Node work?</Accordion.Header>
+                                    <Accordion.Body>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                        culpa qui officia deserunt mollit anim id est laborum.
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                            </Accordion>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div className="container-fluid mt-5 p-0 d-flex justify-content-center align-items-center">
+                <Iframe url="https://www.youtube.com/embed/z05PS1qYu9I"
+                    width="500px"
+                    height="320px"
+                    id=""
+                    className=""
+                    display="block"
+                    position="relative" />
+            </div>
+            <div className="container-fluid mt-5 p-0">
+
+                <Iframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.792482872038!2d90.37337911429776!3d23.790402893165385!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c734c4abc47d%3A0xd696584909ddf06f!2sShewraPara%20Bus%20Stand!5e0!3m2!1sen!2sbd!4v1666757511818!5m2!1sen!2sbd"
+                    width="100%"
+                    height="320px"
+                    padding="0px"
+                    id=""
+                    className=""
+                    display="block"
+                    position="relative" />
             </div>
         </div>
     );
--- src/Page/FaqPage/FaqPage.js
+++ src/Page/FaqPage/FaqPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './FaqPage.css'
+import './FaqPage.css';
+import Accordion from 'react-bootstrap/Accordion';
+import FaqImage from '../../assets/faq.jpg';
 
 const FaqPage = () => {
     return (
@@ -9,11 +11,86 @@ const FaqPage = () => {
                 <div className="container">
                     <div className="pages-banner-content mt-5 mb-5">
                         <h2 className='text-white text-center'>FAQ</h2>
+                        <hr />
                         <div className='breadcrumb d-flex justify-content-center'>
                             <span><Link to={'/home'}>Home</Link></span>
                             <span><Link to={'/blog'}>Blog</Link></span>
                         </div>
                     </div>
+                </div>
+            </div>
+            <div className="container-fluid mt-5">
+                <div className="container ">
+                    <div className='d-flex justify-content-center align-items-center'>
+                        <img className='w-50' src={FaqImage} alt="" />
+                    </div>
+                    <div className='row mb-5 mt-3'>
+                        <div className='col d-flex justify-content-center align-items-center'>
+                            <Accordion defaultActiveKey={['0']} alwaysOpen className="w-50">
+                                <Accordion.Item eventKey="0">
+                                    <Accordion.Header> What is cors?</Accordion.Header>
+                                    <Accordion.Body>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                        culpa qui officia deserunt mollit anim id est laborum.
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                                <Accordion.Item eventKey="1">
+                                    <Accordion.Header>Why are you using firebase? </Accordion.Header>
+                                    <Accordion.Body>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                        culpa qui officia deserunt mollit anim id est laborum.
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                                <Accordion.Item eventKey="1">
+                                    <Accordion.Header>What other options do you have to implement authentication? </Accordion.Header>
+                                    <Accordion.Body>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                        culpa qui officia deserunt mollit anim id est laborum.
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                                <Accordion.Item eventKey="2">
+                                    <Accordion.Header> How does the private route work?</Accordion.Header>
+                                    <Accordion.Body>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                        culpa qui officia deserunt mollit anim id est laborum.
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                                <Accordion.Item eventKey="3">
+                                    <Accordion.Header>What is Node? How does Node work?</Accordion.Header>
+                                    <Accordion.Body>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                        culpa qui officia deserunt mollit anim id est laborum.
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                            </Accordion>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
